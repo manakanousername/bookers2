94b05a4b484ca0754cd404cc9ec9fd1abc547b95
EC2 Default User <ec2-user@ip-172-31-43-240.ap-northeast-1.compute.internal>
2024-06-26 12:27:24 +0000

    [202406262127]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :email, :encrypted_password])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  devise_for :user
+  get 'users/edit'
+  get 'users/show'
+  get 'users/index'
+  devise_for :users
+  resources :users, only: [:show, :edit, :index]
   resources :books, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
+  root to: "homes#top"  ##homes/topをトップページとして表示
 end
